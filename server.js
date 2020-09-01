32f1c2842c14a45a3b2d59f6250a51b071ea1f34
allen3680 <allen3680@gmail.com>
2020-09-01 21:50:14 +0800

    first

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.listen(3000);
 
-app.use(express.static('public'));
+app.use(express.static('view'));
 
 app.get('/', (req, res) => {
   res.redirect('http://oceal.co/index.html');
