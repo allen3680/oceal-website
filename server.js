ad05a695d780291bdad18c1537f45bf43d361e18
allen3680 <allen3680@gmail.com>
2020-09-02 20:00:03 +0800

    change foldername

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.listen(3000);
 
-app.use(express.static('view'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.redirect('http://oceal.co/index.html');
