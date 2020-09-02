a6b9d2da831dd67798263c2625181956990be118
allen3680 <allen3680@gmail.com>
2020-09-03 00:10:11 +0800

    ppp

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`listen on port: ${port}`);
+});
 
-app.use(express.static('public'));
+app.use(express.static('view'));
 
 app.get('/', (req, res) => {
   res.redirect('http://oceal.co/index.html');
