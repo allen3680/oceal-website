d17f278329e032cba700c9d1671167372ee65f1e
allen3680 <allen3680@gmail.com>
2020-09-03 00:13:02 +0800

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.listen(3000, () => {
-  console.log(`listen on port: ${port}`);
+  console.log(`listen on port: 3000`);
 });
 
 app.use(express.static('view'));
